922cde6c4c63d2e0ffdae0080edb07b03ee6d27d
lhasmi <hasmilaila@gmail.com>
2024-01-04 19:02:10 +0100

    train

---
+++ train/exam_05/2/cpp_module02/TargetGenerator.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "ATarget.hpp"
+#include <map>
+
+class TargetGenerator
+{
+	public:
+		TargetGenerator();
+		~TargetGenerator();
+
+		void learnTargetType(ATarget *target_ptr);
+		void forgetTargetType(std::string const &name);
+		ATarget* createTarget(std::string const &name);
+
+	private:
+		std::map<std::string, ATarget *> arr_target;
+
+		TargetGenerator(TargetGenerator const &other);
+		TargetGenerator &operator=(TargetGenerator const &other);
+};
